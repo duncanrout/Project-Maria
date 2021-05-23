b160b89f2f1edccf4e7a995a2bd0495d93f0b098
ladams9 <ladams9@luc.edu>
2021-05-23 16:17:02 -0500

    edited App.js and added /pages/home.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {Home} from './pages/home.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div id="page-body">
+          <Switch>
+            <Route path="/home" component={Home} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ app/src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const Home = (props) => (
+    <div>
+        home page!
+    </div>
+  );
+
+
+export default Home;
