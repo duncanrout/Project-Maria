e6701938eb0597c6d5fd895624232cbff93e1015
tabbenhaus <tabbenhaus@luc.edu>
2021-05-26 19:36:02 -0500

    spongebob why addition

--- app/src/pages/about.js
+++ app/src/pages/about.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
+alert("this is gonna be the about page")
+   
 
 export const About = (props) => (
     <div>
         <header>
-        ABOUT US
-        AMONG US
-            </header>
+            ABOUT US
+            AMONG US
+        </header>
+        <body>
+            <b1> WHY SPONGEBOB </b1>
+        </body>
     </div>
+
 );
 
 
